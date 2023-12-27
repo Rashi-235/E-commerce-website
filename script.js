fe6a4fd3bea23aa27c4b33f2badac517fd6e14cd
Rashi-235 <124442216+Rashi-235@users.noreply.github.com>
2023-12-28 01:50:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// hamburger menu
+const bar=document.getElementById('bar');
+const close=document.getElementById('close');
+const nav=document.getElementById('navb');
+
+if(bar){
+    bar.addEventListener('click', ()=>{
+        nav.classList.add('active');
+    })
+}
+
+if(close){
+    close.addEventListener('click', ()=>{
+        nav.classList.remove('active');
+    })
+}
