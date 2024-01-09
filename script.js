a699bf7d6188543ff7247e3fcc85d4128f67a7d6
Rashi Agrawal <agrawalrashi73@gmail.com>
2024-01-09 09:22:51 +0530

    created image slider

--- script.js
+++ script.js
@@ -16,6 +16,34 @@ if(close){
 }
 
 
+
+//image slider
+const slides=document.querySelectorAll('.slide');
+var counter=0;
+slides.forEach((slide, index)=>{
+    slide.style.left=`${index*100}%`;
+})
+
+const goPrev=()=>{
+    counter--;
+    slideImage();
+}
+
+const goNext=()=>{
+    if(counter<slides.length-1){
+    counter++;
+    slideImage();}
+}
+
+const slideImage=()=>{
+    slides.forEach((slide)=>{
+        slide.style.transform=`translateX(-${counter*100}%)`;
+    }
+    )}
+
+
+
+
 //cart page
 
 let cart=document.querySelector(".cart");
